5c2d4df8f25c9aad6de94e83b3910afcea3d4862
llinares <lean8086@gmail.com>
2014-02-27 21:49:22 -0300

    Add docs

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var toRadians = Math.PI / 180,
     toDegrees = 180 / Math.PI;
 
-module.exports.degToRad = function (deg) {
+/**
+ * Convert degrees to radians.
+ * @param {Number} deg
+ * @return {Number}
+ * @api public
+ */
+exports.degToRad = function (deg) {
     return deg * toRadians;
 };
 
-module.exports.radToDeg = function (rad) {
+/**
+ * Convert radians to degrees.
+ * @param {Number} rad
+ * @return {Number}
+ * @api public
+ */
+exports.radToDeg = function (rad) {
     return rad * toDegrees;
 };
